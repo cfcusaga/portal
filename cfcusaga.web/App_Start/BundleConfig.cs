7ab74173b1f102153b52d5f85ffcfa780fb3b5f0
arankun <arankun@yahoo.com>
2016-03-13 10:36:30 -0400

    fix the bootstrap offset error. add the scripts in layout.cshtml

--- cfcusaga.web/App_Start/BundleConfig.cs
+++ cfcusaga.web/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace  Cfcusaga.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      //"~/Content/carousel.css",
+                      "~/Content/PagedList1.css",
                       "~/Content/simple-sidebar.css",
                       "~/Content/facebook.css",
                       "~/Content/variables.less",
