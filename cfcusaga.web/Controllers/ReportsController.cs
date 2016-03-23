c04f1994cde8ac4aa789df36af29c19340f1d0cf
arankun <arankun@yahoo.com>
2016-03-23 11:16:56 -0400

    Added Registrations report page that is public

--- cfcusaga.web/Controllers/ReportsController.cs
+++ cfcusaga.web/Controllers/ReportsController.cs
@@ -4,9 +4,11 @@ using System.Web.Mvc;
 using cfcusaga.data;
 using cfcusaga.domain.Helpers;
 using Cfcusaga.Web.Models;
+using Enums = cfcusaga.domain.Enums;
 
 namespace Cfcusaga.Web.Controllers
 {
+    [Authorize(Roles = "SuperUser, Admin")]
     public class ReportsController : Controller
     {
         private PortalDbContext _db = new PortalDbContext();
@@ -59,6 +61,62 @@ namespace Cfcusaga.Web.Controllers
             orders = FilterItemsBy(searchString, orders);
             orders = SortItemsBy(sortOrder, orders);
             const int pageSize = 30;
+            var pageNumber = (page ?? 1);
+            return View(await orders.ToPagedListAsync(pageNumber, pageSize));
+
+        }
+
+        [AllowAnonymous]
+        [HttpGet()]
+        [Route("registrations")]
+        public async Task<ActionResult> Registrations(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            const int eventId = 7;
+            InitIndexViewBag(sortOrder);
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var orders = from o in _db.Orders.AsNoTracking()
+                         join od in _db.OrderDetails.AsNoTracking() on o.OrderId equals od.OrderId
+                         join i in _db.Items.AsNoTracking() on od.ItemId equals i.ID
+                         join e in _db.Events.AsNoTracking() on i.EventId equals e.Id
+                         where e.Id == eventId
+                         && i.CatagoryID == (int)CategoryTypeEnum.Registration
+                         //orderby o.OrderId descending 
+                         select new OrderItems
+                         {
+                             OrderId = o.OrderId,
+                             OrderDate = o.OrderDate,
+                             OrderDetailId = od.OrderDetailId,
+                             ItemId = i.ID,
+                             ItemName = i.Name,
+                             CategoryId = i.CatagoryID,
+                             Firstname = od.Firstname,
+                             Lastname = od.Lastname,
+                             BirthDate = od.BirthDate,
+                             OrderByLastname = o.LastName,
+                             OrderByFirstname = o.FirstName,
+                             TshirtSize = od.TshirtSize,
+                             Allergies = od.Allergies,
+                             City = o.City,
+                             State = o.State,
+                             ZipCode = o.PostalCode,
+                             Phone = o.Phone,
+                             Price = od.UnitPrice
+                         }
+            ;
+
+            orders = FilterItemsBy(searchString, orders);
+            orders = SortItemsBy(sortOrder, orders);
+            const int pageSize = 50;
             var pageNumber = (page ?? 1);
             return View(await orders.ToPagedListAsync(pageNumber, pageSize));
 
