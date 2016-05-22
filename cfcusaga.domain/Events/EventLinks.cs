67cf9fb9308b74b4d04c12c3b4026aebf00553ea
arankun <arankun@yahoo.com>
2016-05-22 00:44:00 -0400

    add file

---
+++ cfcusaga.domain/Events/EventLinks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cfcusaga.domain.Events
+{
+    public class EventInfo
+    {
+        public List<EventLink> Links { get; set; }
+    }
+
+    public class EventLink
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
